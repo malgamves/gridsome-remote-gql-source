30ff02cb5614ee950779c6194c0d186e28e622bf
Daniel Phiri <malgamves@gmail.com>
2019-11-27 12:33:29 +0200

    init

---
+++ gridsome.config.js
@@ -0,0 +1,21 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: 'gridsome-source-graphql',
+      options: {
+        url: 'https://dovue.herokuapp.com/v1alpha1/graphql',
+        fieldName: 'booksList',
+        typeName: 'booksListType',
+
+        headers: {},
+      },
+    },
+  ],
+}
---
+++ src/pages/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <h1>Books Read by Me</h1>
+    <div v-for="book in $page.books" v-bind:key="book.id">
+      <li>{{ book.author}} - {{ book.name}}</li>
+    </div>
+  </Layout>
+</template>
+
+
+<page-query>  
+query {
+  books {
+    author
+    id
+    name
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: "About us"
+  }
+};
+</script>
